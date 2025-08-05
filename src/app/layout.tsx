939e6f225ba486adc08c115a752a03096d9ef439
mateus lira <mateuslira996@gmail.com>
2025-08-05 23:38:57 +0000

    Error: A tree hydrated but some attributes of the server rendered HTML d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="pt-BR" className="dark notranslate" translate="no">
       <head>
+        <meta name="google" content="notranslate" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
