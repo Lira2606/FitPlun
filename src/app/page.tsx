e9af049e588a37b5a675913d8590d2377193b12c
mateus lira <mateuslira996@gmail.com>
2025-08-09 16:35:50 +0000

    coloque esse fundo cinza mais transparente para que de pra visualizar o

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -148,7 +148,7 @@ export default function Home() {
                                                       <span className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                                                           <svg className="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M14.4 14.4 9.6 9.6"/><path d="M18.657 5.343a2 2 0 1 0-2.829-2.829l-1.414 1.414-1.414 1.414 2.829 2.829 1.414-1.414z"/><path d="m19.071 19.071-4.242-4.242"/><path d="m5.343 18.657-1.414 1.414a2 2 0 1 0 2.829 2.829l1.414-1.414"/><path d="M9.6 14.4 4.929 9.67a1 1 0 0 0-1.414 0l-2.121 2.121a1 1 0 0 0 0 1.414l4.671 4.671a1 1 0 0 0 1.414 0l2.121-2.121a1 1 0 0 0 0-1.414Z"/><path d="M14.33 4.929a1 1 0 0 0 0 1.414l2.121 2.121a1 1 0 0 0 1.414 0l4.671-4.671a1 1 0 0 0 0-1.414l-2.121-2.121a1 1 0 0 0-1.414 0Z"/></svg>
                                                       </span>
-                                                      <input type="text" id="exercise-name" name="exercise-name" placeholder="Ex: Supino Reto" required className="w-full bg-gray-700 border border-gray-600 rounded-lg pl-10 pr-4 py-2.5 text-white focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:border-cyan-500" />
+                                                      <input type="text" id="exercise-name" name="exercise-name" placeholder="Ex: Supino Reto" required className="w-full bg-gray-700/80 border border-gray-600 rounded-lg pl-10 pr-4 py-2.5 text-white focus:bg-gray-700/80 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:border-cyan-500" />
                                                   </div>
                                               </div>
                                               <div className="grid grid-cols-2 gap-4">
@@ -158,7 +158,7 @@ export default function Home() {
                                                           <span className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                                                               <svg className="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" /></svg>
                                                           </span>
-                                                          <input type="number" id="exercise-sets" name="exercise-sets" placeholder="Ex: 4" required className="w-full bg-gray-700/80 border border-gray-600 rounded-lg pl-10 pr-4 py-2.5 text-white focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:border-cyan-500 transition-all duration-200" />
+                                                          <input type="number" id="exercise-sets" name="exercise-sets" placeholder="Ex: 4" required className="w-full bg-gray-700/80 border border-gray-600 rounded-lg pl-10 pr-4 py-2.5 text-white focus:bg-gray-700/80 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:border-cyan-500 transition-all duration-200" />
                                                       </div>
                                                   </div>
                                                   <div>
@@ -167,7 +167,7 @@ export default function Home() {
                                                           <span className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                                                               <svg className="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0011.664 0l3.18-3.185m-3.18 3.182v-4.992m0 0h-4.992m4.992 0l-3.181-3.182a8.25 8.25 0 00-11.664 0l-3.18 3.185" /></svg>
                                                           </span>
-                                                          <input type="text" id="exercise-reps" name="exercise-reps" placeholder="Ex: 8-12" required className="w-full bg-gray-700/80 border border-gray-600 rounded-lg pl-10 pr-4 py-2.5 text-white focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:border-cyan-500 transition-all duration-200" />
+                                                          <input type="text" id="exercise-reps" name="exercise-reps" placeholder="Ex: 8-12" required className="w-full bg-gray-700/80 border border-gray-600 rounded-lg pl-10 pr-4 py-2.5 text-white focus:bg-gray-700/80 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:border-cyan-500 transition-all duration-200" />
                                                       </div>
                                                   </div>
                                               </div>
@@ -178,7 +178,7 @@ export default function Home() {
                                                           <span className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                                                               <svg className="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M12 3v17.25m0 0c-1.472 0-2.882.265-4.185.75M12 20.25c1.472 0 2.882.265 4.185.75M18.75 4.97A48.416 48.416 0 0012 4.5c-2.291 0-4.545.16-6.75.47m13.5 0c1.01.143 2.01.317 3 .52m-3-.52l2.62 10.726c.122.499-.106 1.028-.589 1.202a5.988 5.988 0 01-2.036.243c-2.132 0-4.14-.818-5.62-2.24l-2.62-2.62a5.988 5.988 0 01-1.68-4.243V6.345c0-1.22.67-2.312 1.719-2.816.52-.252 1.07-.432 1.64-.563m13.5 0l2.62 10.726c.122.499-.106 1.028-.589 1.202a5.988 5.988 0 01-2.036.243c-2.132 0-4.14-.818-5.62-2.24l-2.62-2.62a5.988 5.988 0 01-1.68-4.243V6.345c0-1.22.67-2.312 1.719-2.816.52-.252 1.07-.432 1.64-.563" /></svg>
                                                           </span>
-                                                          <input type="text" id="exercise-weight" name="exercise-weight" placeholder="Ex: 40kg" className="w-full bg-gray-700/80 border border-gray-600 rounded-lg pl-10 pr-4 py-2.5 text-white focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:border-cyan-500 transition-all duration-200" />
+                                                          <input type="text" id="exercise-weight" name="exercise-weight" placeholder="Ex: 40kg" className="w-full bg-gray-700/80 border border-gray-600 rounded-lg pl-10 pr-4 py-2.5 text-white focus:bg-gray-700/80 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:border-cyan-500 transition-all duration-200" />
                                                       </div>
                                                   </div>
                                                   <div>
@@ -187,7 +187,7 @@ export default function Home() {
                                                           <span className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                                                               <svg className="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                                                           </span>
-                                                          <input type="text" id="exercise-time" name="exercise-time" placeholder="Ex: 30s" className="w-full bg-gray-700/80 border border-gray-600 rounded-lg pl-10 pr-4 py-2.5 text-white focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:border-cyan-500 transition-all duration-200" />
+                                                          <input type="text" id="exercise-time" name="exercise-time" placeholder="Ex: 30s" className="w-full bg-gray-700/80 border border-gray-600 rounded-lg pl-10 pr-4 py-2.5 text-white focus:bg-gray-700/80 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:border-cyan-500 transition-all duration-200" />
                                                       </div>
                                                   </div>
                                               </div>
@@ -197,7 +197,7 @@ export default function Home() {
                                                       <span className="pointer-events-none absolute inset-y-0 left-0 flex items-start pl-3 pt-3">
                                                           <svg className="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" /></svg>
                                                       </span>
-                                                      <textarea id="exercise-notes" name="exercise-notes" rows={3} placeholder="Ex: Aumentar a carga..." className="w-full bg-gray-700/80 border border-gray-600 rounded-lg pl-10 pr-4 py-2.5 text-white focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:border-cyan-500 transition-all duration-200"></textarea>
+                                                      <textarea id="exercise-notes" name="exercise-notes" rows={3} placeholder="Ex: Aumentar a carga..." className="w-full bg-gray-700/80 border border-gray-600 rounded-lg pl-10 pr-4 py-2.5 text-white focus:bg-gray-700/80 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:border-cyan-500 transition-all duration-200"></textarea>
                                                   </div>
                                               </div>
                                             </div>
