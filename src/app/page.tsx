9a692cf480b5de9cf03eecbdb919de5efcf08802
mateus lira <mateuslira996@gmail.com>
2025-08-09 16:22:06 +0000

    quando seleciono adcionar treino ainda pisca o campo de nome do exercici

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -148,7 +148,7 @@ export default function Home() {
                                                       <span className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                                                           <svg className="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M14.4 14.4 9.6 9.6"/><path d="M18.657 5.343a2 2 0 1 0-2.829-2.829l-1.414 1.414-1.414 1.414 2.829 2.829 1.414-1.414z"/><path d="m19.071 19.071-4.242-4.242"/><path d="m5.343 18.657-1.414 1.414a2 2 0 1 0 2.829 2.829l1.414-1.414"/><path d="M9.6 14.4 4.929 9.67a1 1 0 0 0-1.414 0l-2.121 2.121a1 1 0 0 0 0 1.414l4.671 4.671a1 1 0 0 0 1.414 0l2.121-2.121a1 1 0 0 0 0-1.414Z"/><path d="M14.33 4.929a1 1 0 0 0 0 1.414l2.121 2.121a1 1 0 0 0 1.414 0l4.671-4.671a1 1 0 0 0 0-1.414l-2.121-2.121a1 1 0 0 0-1.414 0Z"/></svg>
                                                       </span>
-                                                      <input type="text" id="exercise-name" name="exercise-name" placeholder="Ex: Supino Reto" required className="w-full bg-gray-700/80 border border-gray-600 rounded-lg pl-10 pr-4 py-2.5 text-white focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:border-cyan-500 transition-all duration-200" />
+                                                      <input type="text" id="exercise-name" name="exercise-name" placeholder="Ex: Supino Reto" required className="w-full bg-gray-700 border border-gray-600 rounded-lg pl-10 pr-4 py-2.5 text-white focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:border-cyan-500" />
                                                   </div>
                                               </div>
                                               <div className="grid grid-cols-2 gap-4">
@@ -293,3 +293,5 @@ export default function Home() {
         </>
     );
 }
+
+    
