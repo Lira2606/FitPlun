cf002dc35cd4ccffb13310a1e2785baca8214f5c
mateus lira <mateuslira996@gmail.com>
2025-08-13 00:49:35 +0000

    altere a cor do nome força para mesma cor do nome monte seu treino

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1177,13 +1177,13 @@ export default function Home() {
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 8.25v-1.5a2.25 2.25 0 00-4.5 0v1.5m4.5 0v4.5m-4.5-4.5v4.5m0-6.75h4.5m-4.5 0a2.25 2.25 0 01-2.25-2.25V6.75a2.25 2.25 0 012.25-2.25h4.5a2.25 2.25 0 012.25 2.25v1.5a2.25 2.25 0 01-2.25 2.25m-4.5 0h4.5m-12 6.75a2.25 2.25 0 012.25-2.25h13.5a2.25 2.25 0 012.25 2.25v1.5a2.25 2.25 0 01-2.25 2.25H4.5a2.25 2.25 0 01-2.25-2.25v-1.5z" />
                                 </svg>
                                  <div className="absolute top-0 left-0 w-full h-full icon-fill-animation">
-                                    <svg className="w-full h-full text-emerald-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                                    <svg className="w-full h-full text-cyan-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 8.25v-1.5a2.25 2.25 0 00-4.5 0v1.5m4.5 0v4.5m-4.5-4.5v4.5m0-6.75h4.5m-4.5 0a2.25 2.25 0 01-2.25-2.25V6.75a2.25 2.25 0 012.25-2.25h4.5a2.25 2.25 0 012.25 2.25v1.5a2.25 2.25 0 01-2.25 2.25m-4.5 0h4.5m-12 6.75a2.25 2.25 0 012.25-2.25h13.5a2.25 2.25 0 012.25 2.25v1.5a2.25 2.25 0 01-2.25 2.25H4.5a2.25 2.25 0 01-2.25-2.25v-1.5z" />
                                     </svg>
                                 </div>
                             </div>
                             <h1 className="text-4xl md:text-5xl font-black tracking-wider uppercase splash-title-animation">
-                                <span className="text-emerald-400">Força</span> & Foco
+                                <span className="text-cyan-400">Força</span> & Foco
                             </h1>
                             <p className="mt-2 text-lg text-gray-400 splash-slogan-animation">Sua jornada começa agora.</p>
                             <p id="quote" className="text-center text-gray-300 text-sm italic mt-6 h-5 splash-quote-animation">
